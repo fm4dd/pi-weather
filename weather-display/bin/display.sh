f9d4111122d14be55005ee14895e838cc8aa66b7
fm4dd <public@frank4dd.com>
2022-12-04 12:49:38 +0900

    error-handling for pi-display

--- weather-display/bin/display.sh
+++ weather-display/bin/display.sh
@@ -5,7 +5,7 @@
 # This script is run via cron in 1-minute intervals. It
 # connects with scp to the local Pi weather station and
 # collects that latest sensor.txt plus the three daily_
-# images containing the latest temperature, humidity and
+# images containing the latest temperature, humidity and 
 # pressure graphs.
 #
 # Before it begins, it decides if the TFT display needs
@@ -25,13 +25,13 @@ HOME="/home/pi/pi-display"
 #echo "Brightness (Lux): $LUX"
 
 ##########################################################
-# Get the current TFT display power state
+# Get the current TFT display power state 
 ##########################################################
 DISPLAY=`vcgencmd display_power`
 echo "Display State: $DISPLAY"
 
 ##########################################################
-# Update the 20x4 character LCD
+# Update the 20x4 character LCD 
 ##########################################################
 python $HOME/bin/sensor.py
 
@@ -53,9 +53,9 @@ python $HOME/bin/sensor.py
 
 DISPLAY=`vcgencmd display_power`
 if [ "$DISPLAY" == "display_power=0" ]; then
-   echo "Start day mode, TFT display on"
-   vcgencmd display_power 1 > /dev/null
-   DISPLAY=`vcgencmd display_power`
+  echo "Start day mode, TFT display on"
+  vcgencmd display_power 1 > /dev/null
+  DISPLAY=`vcgencmd display_power`
 fi
 
 ##########################################################
@@ -66,8 +66,20 @@ fi
 ##########################################################
 if [ "$DISPLAY" == "display_power=1"  ]; then
    #scp pi@192.168.11.244:/home/pi/pi-ws01/var/sensor.txt $HOME/var
-   scp pi@192.168.11.244:/home/pi/pi-ws01/web/images/daily_*.png $HOME/var
-   convert \( $HOME/var/daily_temp.png $HOME/var/daily_humi.png $HOME/var/daily_bmpr.png -append \) +append $HOME/var/out.png
+   #scp pi@192.168.11.244:/home/pi/pi-ws01/web/images/daily_*.png $HOME/var
+
+   # Retrieve weather images, and store errors into 'err'
+   err=$(scp pi@192.168.11.244:/home/pi/pi-ws01/web/images/daily_*.png $HOME/var 2>&1 >/dev/null)
+
+   if [ -z "$err" ]; then
+      convert \( $HOME/var/daily_temp.png $HOME/var/daily_humi.png $HOME/var/daily_bmpr.png -append \) +append $HOME/var/out.png
+   else
+      # display the error on the graphic screen
+      rm /home/pi/pi-display/var/daily_temp.png
+      convert -size 800x480 xc:black $HOME/var/daily_temp.png
+      convert $HOME/var/daily_temp.png -fill white -pointsize 20 -annotate +0+50 "$err" $HOME/var/out.png
+   fi
+
    ln -f -s $HOME/var/out.png $HOME/var/out-a1.png
    ln -f -s $HOME/var/out.png $HOME/var/out-a2.png
 fi
@@ -76,8 +88,8 @@ fi
 # If the TFT display is "on" check if we need to start fbi
 ##########################################################
 if [ "$DISPLAY" == "display_power=1" ] && [ ! "$(pidof fbi)" ]; then
-   echo "Starting linux frame buffer fbi"
-   sudo fbi -t 60 -cachemem 0 --autozoom --noverbose -d /dev/fb0 --vt 1 $HOME/var/out.png $HOME/var/out-a1.png $HOME/var/out-a2.png
+  echo "Starting linux frame buffer fbi"
+  sudo fbi -t 60 -cachemem 0 --autozoom --noverbose -d /dev/fb0 --vt 1 $HOME/var/out.png $HOME/var/out-a1.png $HOME/var/out-a2.png
 fi
 ##########################################################
 # End of display.sh
--- weather-display/bin/sensor.py
+++ weather-display/bin/sensor.py
@@ -5,6 +5,8 @@
 ##########################################################
 # sensor.py 20170930 Frank4DD
 # updated 20220425 for JSON data retrieval over HTTP
+# updated 20221204 for weather station access errors
+#
 # This python3 script manages the 20x4 character LCD
 # in combination with the I2C_LCD_driver.py driver.
 ########################################################
@@ -13,17 +15,36 @@ import time
 import I2C_LCD_driver
 from time import *
 import urllib.request
+from urllib.error import URLError, HTTPError
+from datetime import datetime
 import json
 
-sensor_data = urllib.request.urlopen("http://192.168.11.244/getsensor.json").read().decode('utf-8')
-#print(sensor_data)
-sensor_obj = json.loads(sensor_data)
-local_time = localtime(sensor_obj["time"])
-timestr = strftime("%D %H:%M", local_time)
-line1 = "Date: " + timestr;
-line2 = "Temp. "+'\xDF'+"C: " + "{0:.2f}".format(sensor_obj["temp"]) + '\xDF'+ "C"
-line3 = "Humidity: " + "{0:.2f}".format(sensor_obj["humi"]) + "%"
-line4 = "Pressure: " + "{0:.2f}".format(sensor_obj["pres"]/100) + "hPa"
+# Weather station dowload URL for jsaon sensor data file
+ws_url = "http://192.168.11.244/getsensor.json"
+
+try:
+   sensor_data = urllib.request.urlopen(ws_url).read().decode('utf-8')
+except HTTPError as e:
+   err_str = "Error trying to read from " + ws_url + " return code: " + str(e.code)
+   print(err_str)
+   
+try:
+   err_str
+   now = datetime.now() # current date and time
+   line1 = now.strftime("%m/%d/%Y %H:%M:%S")
+   line2 = "Error get sensordata"
+   line3 = "at 192.168.11.244"
+   line4 = "retry in 1 minute"
+except NameError:
+   #print(sensor_data)
+   sensor_obj = json.loads(sensor_data)
+   local_time = localtime(sensor_obj["time"])
+   timestr = strftime("%D %H:%M", local_time)
+   line1 = "Date: " + timestr;
+   line2 = "Temp. "+'\xDF'+"C: " + "{0:.2f}".format(sensor_obj["temp"]) + '\xDF'+ "C"
+   line3 = "Humidity: " + "{0:.2f}".format(sensor_obj["humi"]) + "%"
+   line4 = "Pressure: " + "{0:.2f}".format(sensor_obj["pres"]/100) + "hPa"
+
 #print(line1)
 #print(line2)
 #print(line3)
